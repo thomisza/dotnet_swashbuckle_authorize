821a2215b6cc82c3df1581387147682bb6f74d5a
Thomas ZAWISZA <thomas.zawisza@octo.com>
2022-02-14 10:54:59 +0100

    initial commit

---
+++ openapi_dotnet/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.Identity.Web;
+
+namespace openapi_dotnet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration, "AzureAd");
+
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Scheme = "Bearer",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        
+                        AuthorizationCode = new OpenApiOAuthFlow()
+                        {
+                            AuthorizationUrl = new Uri("https://login.microsoftonline.com/<TENANT_ID>/oauth2/v2.0/authorize"),
+                            TokenUrl = new Uri("https://login.microsoftonline.com/<TENANT_ID>/oauth2/v2.0/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "SCOPE1", "description scope 1" },
+                                { "SCOPE2", "description scope 2" }
+                            }
+                        }
+                    },
+                    
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {                            
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "oauth2"
+                            },
+                        },
+
+                        new [] { "SCOPE_1", "SCOPE_2", }
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.OAuth2RedirectUrl("<REDIRECT_URI>"); //should look like this : https://localhost:5001/swagger/oauth2-redirect.html
+                    c.OAuthClientId("<CLIENT_ID_APP_REGISTRATION_SWAGGER");
+                    c.OAuthUsePkce();
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
